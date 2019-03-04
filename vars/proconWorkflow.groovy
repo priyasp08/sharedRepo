f2cc937ae350c43692aac43754dfed1afeac2145
Shanmugapriya Manickaraj (RBEI/BSI7) <hsm4cob@bosch.com>
2019-03-04 12:57:51 +0530

    Shared repo created

---
+++ vars/proconWorkflow.groovy
@@ -0,0 +1,65 @@
+#!/usr/bin/env groovy
+//Jenkins Proocn Flow
+
+import hudson.model.*
+import hudson.EnvVars
+import groovy.json.JsonSlurperClassic
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import java.net.URL
+import org.jenkinsci.plugins.gitclient.Git;
+import org.jenkinsci.plugins.gitclient.GitClient;
+
+def call(body) {
+	// evaluate the body block, and collect configuration into the object
+	def config = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()
+try {
+node {
+ 
+ stage('Checkout'){
+  echo "Git Checkout"
+  checkout scm
+ }
+ stage('Build'){
+  def mvnHome = tool 'Maven-3.6'
+  //def javahome = tool 'openjdk'
+  sh("${mvnHome}/bin/mvn -B test -Dmaven.test.skip=true")
+  }
+ stage('SonarQube Analysis'){
+  echo "Hi Sonar"
+  withSonarQubeEnv('sonar-6'){
+   def mvnHome = tool 'Maven-3.6'
+   sh("${mvnHome}/bin/mvn sonar:sonar")
+  }
+  }
+} 
+ 
+} 
+catch (exc) {
+/*
+ err = caughtError
+ currentBuild.result = "FAILURE"
+ String recipient = 'infra@lists.jenkins-ci.org'
+ mail subject: "${env.JOB_NAME} (${env.BUILD_NUMBER}) failed",
+         body: "It appears that ${env.BUILD_URL} is failing, somebody should do something about that",
+           to: recipient,
+      replyTo: recipient,
+ from: 'noreply@ci.jenkins.io'
+*/
+} finally {
+  
+ (currentBuild.result != "ABORTED") && node("master") {
+     // Send e-mail notifications for failed or unstable builds.
+     // currentBuild.result must be non-null for this step to work.
+     //step([$class: 'Mailer',
+     //   notifyEveryUnstableBuild: true,
+     //   recipients: "${email_to}",
+     //   sendToIndividuals: true])
+ }
+}
+}	
+	
+	
